1611f9628b8cbfa2552e47d31677862c4c9a26e0
John Michael Ocol <158814987+JohnMichaelOcol@users.noreply.github.com>
2025-02-21 12:57:55 +0800

    Add files via upload

---
+++ Hands-on2/hands-on2.js
@@ -0,0 +1,6 @@
+function addnumbers() {
+   var a = Number(prompt("Enter first number: "));
+   var b = Number(prompt("Enter second Number: "));
+   alert("The sum is " + (a + b));
+}
+addnumbers();
