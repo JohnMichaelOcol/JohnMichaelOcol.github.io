0647df07343e8f4eead1a70f9dd9dd58738ab2d9
John Michael Ocol <158814987+JohnMichaelOcol@users.noreply.github.com>
2025-02-21 13:21:42 +0800

    Add files via upload

---
+++ Hands-on4/act1.js
@@ -0,0 +1,12 @@
+function oddOrEven() {
+   let number = Number(prompt("Enter a number: "));
+   
+   if (number % 2 == 0) {
+      alert(number + " is an Even Number.");
+   } else {
+      alert(number + " is an Odd Number.");
+   }
+}
+oddOrEven();
+
+
